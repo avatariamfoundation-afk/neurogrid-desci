3d276229dc84097f67bce4c725af2508291bb605
Aethera BioSync <avatariamfoundation@gmail.com>
2026-01-28 23:55:32 -0300

    Update funding_intent.py

--- src/funding/funding_intent.py
+++ src/funding/funding_intent.py
@@ -1,1 +1,10 @@
+class FundingIntentLedger:
+    def __init__(self):
+        self.intents = []
+
+    def declare(self, research_id: str, amount: int):
+        self.intents.append({
+            "research_id": research_id,
+            "amount": amount
+        })
 
